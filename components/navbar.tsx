5a14bed51874d09f23e6d8a93214121e06fcee9a
eorev <eorev@udel.edu>
2024-03-02 20:44:38 -0500

    linter changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,11 +22,10 @@ const FlyoutNav = () => {
     <nav
       className={`fixed top-0 z-50 w-full px-6 text-black 
       transition-all duration-300 ease-out lg:px-12
-      ${
-        scrolled
+      ${scrolled
           ? "bg-neutral-950 py-3 shadow-xl"
           : "bg-neutral-950/0 py-6 shadow-none"
-      }`}
+        }`}
     >
       <div className="mx-auto flex max-w-7xl items-center justify-between">
         <Logo />
