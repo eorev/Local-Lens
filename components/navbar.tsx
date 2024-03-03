0231882ba7411e8cfbf6e4c72878ec4766ac50a5
ddeflores <94253526+ddeflores@users.noreply.github.com>
2024-03-03 06:35:32 -0500

    delete pricing, about us, careers, and documentation links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -59,7 +59,6 @@ const FlyoutNav = () => {
       <div className="mx-auto flex max-w-7xl items-center justify-between">
         <Logo color={`${scrolled ? "white" : "black"}`} />
         <div className="hidden gap-6 lg:flex">
-          <Links />
           {!signIn && (
             <button onClick={() => signInWithGoogle()}>Sign in</button>
           )}
@@ -92,18 +91,6 @@ const Logo = ({ color = "white" }: { color?: string }) => {
       </span>
 
       <Image width={50} height={50} src={smallLogo.src} alt="logo" />
-    </div>
-  );
-};
-
-const Links = () => {
-  return (
-    <div className="flex items-center gap-6">
-      {LINKS.map((l) => (
-        <NavLink key={l.text} href={l.href} FlyoutContent={l.component}>
-          {l.text}
-        </NavLink>
-      ))}
     </div>
   );
 };
@@ -152,169 +139,6 @@ const NavLink = ({
           </motion.div>
         )}
       </AnimatePresence>
-    </div>
-  );
-};
-
-const AboutUsContent = () => {
-  return (
-    <div className="grid h-fit w-full grid-cols-12 shadow-xl lg:h-72 lg:w-[600px] lg:shadow-none xl:w-[750px]">
-      <div className="col-span-12 flex flex-col justify-between bg-neutral-950 p-6 lg:col-span-4">
-        <div>
-          <h2 className="mb-2 text-xl font-semibold text-white">About us</h2>
-          <p className="mb-6 max-w-xs text-sm text-neutral-400">
-            Placeholder is the worlds leading placeholder company.
-          </p>
-        </div>
-        <a
-          href="#"
-          className="flex items-center gap-1 text-xs text-indigo-300 hover:underline"
-        >
-          Learn more <FiArrowRight />
-        </a>
-      </div>
-      <div className="col-span-12 grid grid-cols-2 grid-rows-2 gap-3 bg-white p-6 lg:col-span-8">
-        <a
-          href="#"
-          className="rounded border-2 border-neutral-200 bg-white p-3 transition-colors hover:bg-neutral-100"
-        >
-          <h3 className="mb-1 font-semibold">Features</h3>
-          <p className="text-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quam?
-          </p>
-        </a>
-        <a
-          href="#"
-          className="rounded border-2 border-neutral-200 bg-white p-3 transition-colors hover:bg-neutral-100"
-        >
-          <h3 className="mb-1 font-semibold">Testimonials</h3>
-          <p className="text-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quam?
-          </p>
-        </a>
-        <a
-          href="#"
-          className="rounded border-2 border-neutral-200 bg-white p-3 transition-colors hover:bg-neutral-100"
-        >
-          <h3 className="mb-1 font-semibold">Press</h3>
-          <p className="text-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quam?
-          </p>
-        </a>
-        <a
-          href="#"
-          className="rounded border-2 border-neutral-200 bg-white p-3 transition-colors hover:bg-neutral-100"
-        >
-          <h3 className="mb-1 font-semibold">Blog</h3>
-          <p className="text-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quam?
-          </p>
-        </a>
-      </div>
-    </div>
-  );
-};
-
-const PricingContent = () => {
-  return (
-    <div className="w-full bg-white p-6 shadow-none lg:w-[250px] lg:shadow-xl">
-      <div className="grid grid-cols-2 lg:grid-cols-1">
-        <div className="mb-3 space-y-3">
-          <h3 className="font-semibold">For Individuals</h3>
-          <a href="#" className="block text-sm hover:underline">
-            Introduction
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Pay as you go
-          </a>
-        </div>
-        <div className="mb-6 space-y-3">
-          <h3 className="font-semibold">For Companies</h3>
-          <a href="#" className="block text-sm hover:underline">
-            Startups
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            SMBs
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Enterprise
-          </a>
-        </div>
-      </div>
-      <button className="w-full rounded-lg border-2 border-neutral-950 px-4 py-2 font-semibold transition-colors hover:bg-neutral-950 hover:text-white">
-        Contact sales
-      </button>
-    </div>
-  );
-};
-
-const CareersContent = () => {
-  return (
-    <div className="grid w-full grid-cols-12 shadow-xl lg:w-[750px]">
-      <div className="col-span-12 flex flex-col justify-between bg-indigo-600 p-6 lg:col-span-4">
-        <div className="mb-6">
-          <h2 className="mb-2 text-xl font-semibold text-white">Careers</h2>
-          <p className="text-sm text-indigo-100">
-            Placeholder was rated a top place to work by Placeholder.
-          </p>
-        </div>
-        <a
-          href="#"
-          className="flex items-center gap-1 text-xs text-indigo-200 hover:underline"
-        >
-          Careers site <FiArrowRight />
-        </a>
-      </div>
-      <div className="col-span-12 grid grid-cols-2 gap-3 bg-white p-6 lg:col-span-8 lg:grid-cols-3">
-        <div className="space-y-3">
-          <h3 className="font-semibold">Business</h3>
-          <a href="#" className="block text-sm hover:underline">
-            Marketing
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Finance
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Legal
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Sales
-          </a>
-        </div>
-        <div className="space-y-3">
-          <h3 className="font-semibold">Engineering</h3>
-          <a href="#" className="block text-sm hover:underline">
-            Full stack
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Dev ops
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            QA
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Data
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Machine learning
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Management
-          </a>
-        </div>
-        <div className="space-y-3">
-          <h3 className="font-semibold">More</h3>
-          <a href="#" className="block text-sm hover:underline">
-            Support
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Office
-          </a>
-          <a href="#" className="block text-sm hover:underline">
-            Other
-          </a>
-        </div>
-      </div>
     </div>
   );
 };
@@ -410,18 +234,6 @@ const MobileMenu = () => {
                 <FiX className="text-3xl text-neutral-950" />
               </button>
             </div>
-            <div className="h-screen overflow-y-scroll bg-neutral-100 p-6">
-              {LINKS.map((l) => (
-                <MobileMenuLink
-                  key={l.text}
-                  href={l.href}
-                  FoldContent={l.component}
-                  setMenuOpen={setOpen}
-                >
-                  {l.text}
-                </MobileMenuLink>
-              ))}
-            </div>
             <div className="flex justify-end bg-neutral-950 p-6"></div>
           </motion.nav>
         )}
@@ -431,25 +243,3 @@ const MobileMenu = () => {
 };
 
 export default FlyoutNav;
-
-const LINKS = [
-  {
-    text: "About us",
-    href: "#",
-    component: AboutUsContent,
-  },
-  {
-    text: "Pricing",
-    href: "#",
-    component: PricingContent,
-  },
-  {
-    text: "Careers",
-    href: "#",
-    component: CareersContent,
-  },
-  {
-    text: "Documentation",
-    href: "#",
-  },
-];
