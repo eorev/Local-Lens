f325b0bade085077ba1fa9e3201e38292095fb55
StandardGage <gagedolph@yahoo.com>
2024-03-02 22:49:23 -0500

    Refactor components Hero and Grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,83 +5,123 @@ import Link from "next/link";
 import Layout from "../components/layout";
 import Hero from "../components/hero";
 import { AuthContextProvider } from "@/contexts/AuthContext";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { use, useEffect, useState } from "react";
+import { set } from "firebase/database";
+import Grid from "@/components/Grid";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-    // Make sure to handle errors appropriately in a real application
-    async function getPlusCode(coordinates: Number[]) {
-        const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
-        console.log("Coordinates:", coordinates);
+  const [address, setAddress] = useState("Newark, DE, USA");
+  const [zipcode, setZipcode] = useState("19711");
+  const [offices, setOffices] = useState([]);
+  const [officials, setOfficials] = useState([]);
+  // Make sure to handle errors appropriately in a real application
+  async function getPlusCode(coordinates: Number[]) {
+    const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+    console.log("Coordinates:", coordinates);
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates[0]},${coordinates[1]}&key=${API_KEY}&result_type=street_address`
+      );
+      const data = await response.json();
+      console.log("Plus code is:", data.plus_code.compound_code);
+      return data.plus_code.compound_code;
+    } catch (error) {
+      console.error("Failed to get plus code:", error);
+    }
+  }
+
+  async function getAddress(plusCode: String) {
+    const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${plusCode}&key=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log("Address is:", data.results[0].formatted_address);
+      if (data.results[0].formatted_address) {
+        setAddress(data.results[0].formatted_address);
+      }
+    } catch (error) {
+      console.error("Failed to get address:", error);
+    }
+  }
+
+  async function getRepresentatives() {
+    const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/civicinfo/v2/representatives?key=${API_KEY}&address=${address}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setOffices(data.offices);
+      setOfficials(data.officials);
+      // Example processing (commented out)
+      // data.offices.map((office) => console.log(office));
+      // data.officials.map((official) => console.log(official));
+      offices.map((office) => console.log(office));
+      officials.map((official) => console.log(official));
+    } catch (error) {
+      console.error("Failed to get representatives:", error);
+    }
+  }
+
+  async function fetchAll() {
+    if (global.navigator && global.navigator.geolocation) {
+      global.navigator.geolocation.getCurrentPosition(async (position) => {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+        const coordinates = [
+          position.coords.latitude,
+          position.coords.longitude,
+        ];
         try {
-            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates[0]},${coordinates[1]}&key=${API_KEY}&result_type=street_address`);
-            const data = await response.json();
-            console.log("Plus code is:", data.plus_code.compound_code);
-            return data.plus_code.compound_code;
+          const plusCode = await getPlusCode(coordinates);
+          console.log("TEST", plusCode);
+          await getAddress(plusCode);
         } catch (error) {
-            console.error("Failed to get plus code:", error);
+          console.error("Failed in fetchAll:", error);
         }
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
     }
+  }
 
-    async function getAddress(plusCode: String) {
-        const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
-        try {
-            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${plusCode}&key=${API_KEY}`);
-            const data = await response.json();
-            console.log("Address is:", data.results[0].formatted_address);
-            await getRepresentatives(data.results[0].formatted_address);
-        } catch (error) {
-            console.error("Failed to get address:", error);
-        }
-    }
+  useEffect(() => {
+    getAddress(zipcode);
+    getRepresentatives();
+  }, [zipcode]);
 
-    async function getRepresentatives(address: String) {
-        const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
-        try {
-            const response = await fetch(`https://www.googleapis.com/civicinfo/v2/representatives?key=${API_KEY}&address=${address}`);
-            const data = await response.json();
-            console.log(data);
-            // Example processing (commented out)
-            // data.offices.map((office) => console.log(office));
-            // data.officials.map((official) => console.log(official));
-        } catch (error) {
-            console.error("Failed to get representatives:", error);
-        }
-    }
-
-    async function fetchAll() {
-        if (global.navigator && global.navigator.geolocation) {
-            global.navigator.geolocation.getCurrentPosition(async (position) => {
-                console.log("Latitude is :", position.coords.latitude);
-                console.log("Longitude is :", position.coords.longitude);
-                const coordinates = [position.coords.latitude, position.coords.longitude];
-                try {
-                    const plusCode = await getPlusCode(coordinates);
-                    console.log("TEST", plusCode);
-                    await getAddress(plusCode);
-                } catch (error) {
-                    console.error("Failed in fetchAll:", error);
-                }
-            });
-        } else {
-            console.log("Geolocation is not supported by this browser.");
-        }
-    }
-
-
+  useEffect(() => {
     fetchAll();
+    getRepresentatives();
+    officials.map((official) => console.log(official));
+    offices.map((office) => console.log(office));
+  }, []);
 
-    return (
-        <AuthContextProvider>
-            <Layout title="Home" description="The homepage of local lens.">
-                <Head>
-                    <title>Local Lens</title>
-                    <meta
-                        name="description"
-                        content="Understand your politicians and how you can leverage your rights to vote."
-                    />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <Hero />
-            </Layout>
-        </AuthContextProvider>
-    );
+  return (
+    <AuthProvider>
+      <Layout title="Home" description="The homepage of local lens.">
+        <Head>
+          <title>Local Lens</title>
+          <meta
+            name="description"
+            content="Understand your politicians and how you can leverage your rights to vote."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Hero />
+        <Input
+          onChange={(e) => setZipcode(e.target.value)}
+          className="w-1/8 text-center text-lg m-auto rounded-lg text-white"
+          type="email"
+          placeholder="Enter Zipcode"
+        />
+        <Grid />
+      </Layout>
+    </AuthProvider>
+  );
 }
