318ce6ad2c264d0dfbb5b5a4a878eeb38c5bcab1
StandardGage <gagedolph@yahoo.com>
2024-03-02 19:45:05 -0500

    Update layout and navbar styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,6 +7,8 @@ import {
   useScroll,
   motion,
 } from "framer-motion";
+import smallLogo from "@/public/logo-small.png";
+import Image from "next/image";
 
 const FlyoutNav = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -18,7 +20,7 @@ const FlyoutNav = () => {
 
   return (
     <nav
-      className={`fixed top-0 z-50 w-full px-6 text-white 
+      className={`fixed top-0 z-50 w-full px-6 text-black 
       transition-all duration-300 ease-out lg:px-12
       ${
         scrolled
@@ -38,30 +40,15 @@ const FlyoutNav = () => {
   );
 };
 
-const Logo = ({ color = "white" }: { color?: string }) => {
+const Logo = ({ color = "#252329" }: { color?: string }) => {
   // Temp logo from https://logoipsum.com/
   return (
     <div className="flex items-center gap-2">
       <span className="text-2xl font-bold" style={{ color }}>
         Local Lens
       </span>
-      <svg
-        width="50"
-        height="39"
-        viewBox="0 0 50 39"
-        fill={color}
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-10"
-      >
-        <path
-          d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
-          stopColor={color}
-        ></path>
-        <path
-          d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-          stopColor={color}
-        ></path>
-      </svg>
+
+      <Image width={50} height={50} src={smallLogo.src} alt="logo" />
     </div>
   );
 };
