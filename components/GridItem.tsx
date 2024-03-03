19786786590ff1d60b4e0f0af4cfd471ed62073e
eorev <eorev@udel.edu>
2024-03-03 06:19:31 -0500

    account page

--- components/GridItem.tsx
+++ components/GridItem.tsx
@@ -3,72 +3,72 @@ import { CSSProperties } from "react";
 import { FaUserCircle } from "react-icons/fa";
 
 interface GridItemProps {
-  id: number;
-  name: string;
-  party: string;
-  imageUrl: string;
-  office: string;
-  onClick: () => void;
+    id: number;
+    name: string;
+    party: string;
+    imageUrl: string;
+    office: string;
+    onClick: () => void;
 }
 
 const isValidHttpUrl = (string: any) => {
-  let url;
+    let url;
 
-  try {
-    url = new URL(string);
-  } catch (_) {
-    return false;
-  }
+    try {
+        url = new URL(string);
+    } catch (_) {
+        return false;
+    }
 
-  return url.protocol === "http:" || url.protocol === "https:";
+    return url.protocol === "http:" || url.protocol === "https:";
 };
 
 const GridItem: React.FC<GridItemProps> = ({
-  name,
-  party,
-  imageUrl,
-  office,
-  onClick,
+    name,
+    party,
+    imageUrl,
+    office,
+    onClick,
 }) => {
-  const imageWidth = 80;
-  const imageHeight = 80;
+    const imageWidth = 80;
+    const imageHeight = 80;
 
-  const cardStyle: CSSProperties = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "20px",
-    boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
-    borderRadius: "10px",
-    backgroundColor: "white",
-    cursor: "pointer",
-    minWidth: "350px",
-    minHeight: "300px",
-    margin: "10px",
-  };
+    const cardStyle: CSSProperties = {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "20px",
+        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+        borderRadius: "10px",
+        backgroundColor: "white",
+        cursor: "pointer",
+        minWidth: "350px",
+        minHeight: "300px",
+        margin: "10px",
+    };
 
-  return (
-    <div style={cardStyle} onClick={onClick}>
-      {isValidHttpUrl(imageUrl) ? (
-        <img
-          src={imageUrl}
-          alt={name}
-          style={{
-            width: `${imageWidth}px`,
-            height: `${imageHeight}px`,
-            objectFit: "cover",
-            borderRadius: "50%",
-          }}
-        />
-      ) : (
-        <FaUserCircle size={imageWidth} style={{ marginBottom: "8px" }} />
-      )}
-      <h3 style={{ margin: "10px 0 5px 0" }}>{name}</h3>
-      <p style={{ margin: "0", color: "grey" }}>{party}</p>
-      <p style={{ margin: "0", color: "#007BFF" }}>{office}</p>
-    </div>
-  );
+    return (
+        <div style={cardStyle} onClick={onClick}>
+            {isValidHttpUrl(imageUrl) ? (
+                <img
+                    src={imageUrl}
+                    alt={name}
+                    style={{
+                        width: `${imageWidth}px`,
+                        height: `${imageHeight}px`,
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                    }}
+                />
+            ) : (
+                <FaUserCircle size={imageWidth} style={{ marginBottom: "8px" }} />
+            )}
+            <h3 style={{ margin: "10px 0 5px 0" }}>{name}</h3>
+            <p style={{ margin: "0", color: "grey" }}>{party}</p>
+            <p style={{ margin: "0", color: "#007BFF" }}>{office}</p>
+        </div>
+    );
 };
 
 export default GridItem;
--- pages/account.tsx
+++ pages/account.tsx
@@ -1,9 +1,70 @@
-import Layout from "@/components/layout";
+// account.tsx
+"use client";
+import React, { useState, useEffect } from 'react';
+import { onAuthStateChanged, getAuth, signOut, User } from "firebase/auth";
+import Image from 'next/image'; // Ensure you're importing Image from Next.js
 
-export default function Account() {
+const AccountPage = () => {
+  const auth = getAuth();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Handle successful sign out
+      })
+      .catch((error) => {
+        // Handle sign out error
+      });
+  };
+
+  const styles = {
+    container: {
+      textAlign: 'center' as const,
+      marginTop: '50px',
+    },
+    button: {
+      marginTop: '20px',
+      padding: '10px 20px',
+      fontSize: '16px',
+      cursor: 'pointer',
+    },
+  };
+
   return (
-    <Layout title="Account">
-      <h1>Account</h1>
-    </Layout>
+    <div style={styles.container}>
+      {user ? (
+        <div>
+          {/* Use the Image component with required width and height */}
+          {user.photoURL && (
+            <Image
+              src={user.photoURL}
+              alt="User Avatar"
+              width={100} // Set width
+              height={100} // Set height
+              style={{ borderRadius: '50%' }} // Apply styles as needed
+            />
+          )}
+          <h1>Welcome, {user.displayName}</h1>
+          <p>Email: {user.email}</p>
+          <button onClick={handleSignOut} style={styles.button}>Sign Out</button>
+        </div>
+      ) : (
+        <div>
+          <h1>Please sign in</h1>
+          {/* Add sign in form here */}
+        </div>
+      )}
+    </div>
   );
-}
+};
+
+export default AccountPage;
